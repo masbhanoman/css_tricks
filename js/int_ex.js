eab61313961851623ab94d37b0c64c31065d0a1f
masbhanoman <masbhanoman@gmail.com>
2018-02-18 04:05:01 +0600

    this is version 2 v3 on the way

---
+++ js/int_ex.js
@@ -0,0 +1,4 @@
+document.write("Welcome to JS");
+
+
+
---
+++ js/syn.js
@@ -0,0 +1,22 @@
+/*var firstName="Aslam";
+//firstName="Jasim Uddin";
+age=23;
+//age=35;
+//hobby=[1, "rifat", 23, "Shariar", null, undefined];
+info={firstName:"Rahat", age:27};
+document.write("My \"name\" is <b>"+ info.firstName+"</b> & i am <b>"+info.age+"</b> years old"+ " & the type is <b>" + typeof info.agge + "</b>");
+
+*/
+
+/*
+var a=true;
+function rifat()
+{
+    var b=" Rifat ";
+    document.write(b+typeof b +"<br>");
+}
+rifat();
+document.write(a+typeof a);*/
+
+var a=[2,4,6];
+document.getElementById("text").innerHTML= a[2];
